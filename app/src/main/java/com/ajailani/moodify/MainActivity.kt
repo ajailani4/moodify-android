401a8caeb83f7a85d779b686a3a018b2f4d0468f
Ahmad Jailani <ahmadjailani604@gmail.com>
2023-01-02 19:18:42 +0700

    Add welcome page UI

--- app/src/main/java/com/ajailani/moodify/MainActivity.kt
+++ app/src/main/java/com/ajailani/moodify/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.ajailani.moodify.ui.feature.welcome.WelcomeScreen
 import com.ajailani.moodify.ui.theme.MoodifyTheme
 
 class MainActivity : ComponentActivity() {
