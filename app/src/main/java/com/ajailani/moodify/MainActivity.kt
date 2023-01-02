d1facb9273dae2fedc634b9c08164cffe42cd827
Ahmad Jailani <ahmadjailani604@gmail.com>
2023-01-02 18:23:08 +0700

    Add splash screen

--- app/src/main/java/com/ajailani/moodify/MainActivity.kt
+++ app/src/main/java/com/ajailani/moodify/MainActivity.kt
@@ -10,11 +10,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.ajailani.moodify.ui.theme.MoodifyTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
+
         setContent {
             MoodifyTheme {
                 // A surface container using the 'background' color from the theme
