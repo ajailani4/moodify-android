121f201f8c4f4150779713fd06d82eb2071af703
Ahmad Jailani <ahmadjailani604@gmail.com>
2023-01-03 13:30:55 +0700

    Add state management for register screen

--- app/src/main/java/com/ajailani/moodify/ui/feature/register/RegisterScreen.kt
+++ app/src/main/java/com/ajailani/moodify/ui/feature/register/RegisterScreen.kt
@@ -7,33 +7,53 @@ import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.Badge
 import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.material.icons.outlined.Badge
 import androidx.compose.material.icons.outlined.Email
 import androidx.compose.material.icons.outlined.Lock
 import androidx.compose.material.icons.outlined.Person
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.ajailani.moodify.R
+import com.ajailani.moodify.ui.common.UIState
+import com.ajailani.moodify.ui.common.component.ProgressBarWithBackground
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun RegisterScreen(
+    registerViewModel: RegisterViewModel = hiltViewModel(),
     onNavigateUp: () -> Unit
 ) {
+    val onEvent = registerViewModel::onEvent
+    val registerState = registerViewModel.registerState
+    val name = registerViewModel.name
+    val email = registerViewModel.email
+    val username = registerViewModel.username
+    val password = registerViewModel.password
+    val passwordVisibility = registerViewModel.passwordVisibility
+
+    val coroutineScope = rememberCoroutineScope()
     val snackbarHostState = remember { SnackbarHostState() }
+
+    val context = LocalContext.current
 
     Scaffold(
         snackbarHost = { SnackbarHost(hostState = snackbarHostState) }
@@ -65,8 +85,10 @@ fun RegisterScreen(
                 Spacer(modifier = Modifier.height(50.dp))
                 OutlinedTextField(
                     modifier = Modifier.fillMaxWidth(),
-                    value = "",
-                    onValueChange = {},
+                    value = name,
+                    onValueChange = {
+                        onEvent(RegisterEvent.OnNameChanged(it))
+                    },
                     label = {
                         Text(text = stringResource(id = R.string.name))
                     },
@@ -81,8 +103,10 @@ fun RegisterScreen(
                 Spacer(modifier = Modifier.height(20.dp))
                 OutlinedTextField(
                     modifier = Modifier.fillMaxWidth(),
-                    value = "",
-                    onValueChange = {},
+                    value = email,
+                    onValueChange = {
+                        onEvent(RegisterEvent.OnEmailChanged(it))
+                    },
                     label = {
                         Text(text = stringResource(id = R.string.email))
                     },
@@ -92,13 +116,18 @@ fun RegisterScreen(
                             contentDescription = "Email icon"
                         )
                     },
-                    singleLine = true
+                    singleLine = true,
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Email
+                    )
                 )
                 Spacer(modifier = Modifier.height(20.dp))
                 OutlinedTextField(
                     modifier = Modifier.fillMaxWidth(),
-                    value = "",
-                    onValueChange = {},
+                    value = username,
+                    onValueChange = {
+                        onEvent(RegisterEvent.OnUsernameChanged(it))
+                    },
                     label = {
                         Text(text = stringResource(id = R.string.username))
                     },
@@ -113,8 +142,10 @@ fun RegisterScreen(
                 Spacer(modifier = Modifier.height(20.dp))
                 OutlinedTextField(
                     modifier = Modifier.fillMaxWidth(),
-                    value = "",
-                    onValueChange = {},
+                    value = password,
+                    onValueChange = {
+                        onEvent(RegisterEvent.OnPasswordChanged(it))
+                    },
                     label = {
                         Text(text = stringResource(id = R.string.password))
                     },
@@ -125,21 +156,47 @@ fun RegisterScreen(
                         )
                     },
                     trailingIcon = {
-                        Icon(
-                            imageVector = Icons.Default.Visibility,
-                            contentDescription = "Password visibility icon"
-                        )
+                        IconButton(
+                            onClick = {
+                                onEvent(RegisterEvent.OnPasswordVisibilityChanged)
+                            }
+                        ) {
+                            Icon(
+                                imageVector = if (passwordVisibility) {
+                                    Icons.Default.VisibilityOff
+                                } else {
+                                    Icons.Default.Visibility
+                                },
+                                contentDescription = "Password visibility icon"
+                            )
+                        }
                     },
                     singleLine = true,
                     keyboardOptions = KeyboardOptions(
                         keyboardType = KeyboardType.Password
                     ),
-                    visualTransformation = PasswordVisualTransformation()
+                    visualTransformation = if (passwordVisibility) {
+                        VisualTransformation.None
+                    } else {
+                        PasswordVisualTransformation()
+                    }
                 )
                 Spacer(modifier = Modifier.height(30.dp))
                 Button(
                     modifier = Modifier.fillMaxWidth(),
-                    onClick = { /*TODO*/ }
+                    onClick = {
+                        if (name.isNotEmpty() && email.isNotEmpty() &&
+                            username.isNotEmpty() && password.isNotEmpty()
+                        ) {
+                            onEvent(RegisterEvent.Register)
+                        } else {
+                            coroutineScope.launch {
+                                snackbarHostState.showSnackbar(
+                                    context.getString(R.string.fill_the_form)
+                                )
+                            }
+                        }
+                    }
                 ) {
                     Text(
                         modifier = Modifier.padding(5.dp),
@@ -160,12 +217,37 @@ fun RegisterScreen(
                             append(stringResource(id = R.string.login_here))
                         }
                     },
-                    style = MaterialTheme.typography.bodyMedium.copy(
+                    style = MaterialTheme.typography.bodyLarge.copy(
                         color = MaterialTheme.colorScheme.onBackground
                     ),
                     onClick = {}
                 )
             }
+        }
+
+        // Observe register state
+        when (registerState) {
+            UIState.Loading -> ProgressBarWithBackground()
+
+            is UIState.Success -> {}
+
+            is UIState.Fail -> {
+                LaunchedEffect(snackbarHostState) {
+                    registerState.message?.let {
+                        snackbarHostState.showSnackbar(it)
+                    }
+                }
+            }
+
+            is UIState.Error -> {
+                LaunchedEffect(snackbarHostState) {
+                    registerState.message?.let {
+                        snackbarHostState.showSnackbar(it)
+                    }
+                }
+            }
+
+            else -> {}
         }
     }
 }
