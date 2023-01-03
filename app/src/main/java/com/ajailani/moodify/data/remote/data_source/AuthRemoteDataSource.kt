3945b0acbde6065748ea84cab76346b0005aef17
Ahmad Jailani <ahmadjailani604@gmail.com>
2023-01-03 11:48:37 +0700

    Add register function in AuthRemoteDataSource

--- app/src/main/java/com/ajailani/moodify/data/remote/data_source/AuthRemoteDataSource.kt
+++ app/src/main/java/com/ajailani/moodify/data/remote/data_source/AuthRemoteDataSource.kt
@@ -1,4 +1,12 @@
 package com.ajailani.moodify.data.remote.data_source
 
-class AuthRemoteDataSource {
+import com.ajailani.moodify.data.remote.api_service.AuthService
+import com.ajailani.moodify.data.remote.dto.request.RegisterRequest
+import javax.inject.Inject
+
+class AuthRemoteDataSource @Inject constructor(
+    private val authService: AuthService
+) {
+    suspend fun register(registerRequest: RegisterRequest) =
+        authService.register(registerRequest)
 }
