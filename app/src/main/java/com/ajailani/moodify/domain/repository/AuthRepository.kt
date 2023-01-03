e8872fa7adc12134374c89e1d14a7c040a704db8
Ahmad Jailani <ahmadjailani604@gmail.com>
2023-01-03 11:30:01 +0700

    Add domain layer for register feature

--- app/src/main/java/com/ajailani/moodify/domain/repository/AuthRepository.kt
+++ app/src/main/java/com/ajailani/moodify/domain/repository/AuthRepository.kt
@@ -1,4 +1,14 @@
 package com.ajailani.moodify.domain.repository
 
+import android.net.wifi.hotspot2.pps.Credential.UserCredential
+import com.ajailani.moodify.data.Resource
+import kotlinx.coroutines.flow.Flow
+
 interface AuthRepository {
+    fun register(
+        name: String,
+        email: String,
+        username: String,
+        password: String
+    ): Flow<Resource<UserCredential>>
 }
