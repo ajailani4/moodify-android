c367eaab69f154595ca1d301450a681b9968c7e0
Ahmad Jailani <ahmadjailani604@gmail.com>
2023-01-03 11:46:37 +0700

    Add register endpoint and ApiServiceModule

--- app/src/main/java/com/ajailani/moodify/data/remote/api_service/AuthService.kt
+++ app/src/main/java/com/ajailani/moodify/data/remote/api_service/AuthService.kt
@@ -1,4 +1,15 @@
 package com.ajailani.moodify.data.remote.api_service
 
+import com.ajailani.moodify.data.remote.dto.UserCredentialDto
+import com.ajailani.moodify.data.remote.dto.request.RegisterRequest
+import com.ajailani.moodify.data.remote.dto.response.BaseResponse
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.POST
+
 interface AuthService {
+    @POST("register")
+    suspend fun register(
+        @Body registerRequest: RegisterRequest
+    ): Response<BaseResponse<UserCredentialDto>>
 }
