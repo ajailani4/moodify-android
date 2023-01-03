805648596b4a267e2d99e5743b4265c61a68ce65
Ahmad Jailani <ahmadjailani604@gmail.com>
2023-01-03 14:11:20 +0700

    Add unit test for register function in AuthRepository

---
+++ app/src/test/java/com/ajailani/moodify/data/AuthRepositoryTest.kt
@@ -0,0 +1,98 @@
+package com.ajailani.moodify.data
+
+import android.content.Context
+import com.ajailani.moodify.data.remote.data_source.AuthRemoteDataSource
+import com.ajailani.moodify.data.remote.dto.UserCredentialDto
+import com.ajailani.moodify.data.remote.dto.response.BaseResponse
+import com.ajailani.moodify.data.repository.AuthRepositoryImpl
+import com.ajailani.moodify.domain.model.UserCredential
+import com.ajailani.moodify.domain.repository.AuthRepository
+import com.ajailani.moodify.util.userCredential
+import com.ajailani.moodify.util.userCredentialDto
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.runTest
+import okhttp3.ResponseBody.Companion.toResponseBody
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.junit.MockitoJUnitRunner
+import org.mockito.kotlin.any
+import org.mockito.kotlin.doReturn
+import retrofit2.Response
+
+@ExperimentalCoroutinesApi
+@RunWith(MockitoJUnitRunner::class)
+class AuthRepositoryTest {
+
+    @Mock
+    private lateinit var authRemoteDataSource: AuthRemoteDataSource
+
+    @Mock
+    private lateinit var context: Context
+
+    private lateinit var authRepository: AuthRepository
+
+    @Before
+    fun setUp() {
+        authRepository = AuthRepositoryImpl(
+            context,
+            authRemoteDataSource
+        )
+    }
+
+    @Test
+    fun `Register should return success resource`() =
+        runTest(UnconfinedTestDispatcher()) {
+            val response = Response.success(
+                201,
+                BaseResponse(
+                    code = 201,
+                    status = "OK",
+                    data = userCredentialDto
+                )
+            )
+
+            doReturn(response).`when`(authRemoteDataSource).register(any())
+
+            val actualResource = authRepository.register(
+                name = "George",
+                email = "george@email.com",
+                username = "george",
+                password = "123"
+            ).first()
+
+            assertEquals(
+                "Resource should be success",
+                Resource.Success(userCredential),
+                actualResource
+            )
+        }
+
+    @Test
+    fun `Register should return error resource`() =
+        runTest(UnconfinedTestDispatcher()) {
+            val response = Response.error<UserCredentialDto>(
+                409,
+                "".toResponseBody()
+            )
+
+            doReturn(response).`when`(authRemoteDataSource).register(any())
+
+            val actualResource = authRepository.register(
+                name = "George",
+                email = "george@email.com",
+                username = "george",
+                password = "123"
+            ).first()
+
+            assertEquals(
+                "Resource should be error",
+                Resource.Error<UserCredential>(),
+                actualResource
+            )
+        }
+}
--- app/src/test/java/com/ajailani/moodify/util/DummyData.kt
+++ app/src/test/java/com/ajailani/moodify/util/DummyData.kt
@@ -1,5 +1,10 @@
 package com.ajailani.moodify.util
 
+import com.ajailani.moodify.data.remote.dto.UserCredentialDto
 import com.ajailani.moodify.domain.model.UserCredential
 
 val userCredential = UserCredential("a1b2c3")
+val userCredentialDto = UserCredentialDto(
+    username = "george",
+    accessToken = "a1b2c3"
+)
