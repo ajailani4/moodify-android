2cd9b2ab129ed9ff9968da0c12917b8a3d50f088
Ahmad Jailani <ahmadjailani604@gmail.com>
2023-01-05 15:43:40 +0700

    Provide ActivityService in ApiServiceModule

--- app/src/main/java/com/ajailani/moodify/di/ApiServiceModule.kt
+++ app/src/main/java/com/ajailani/moodify/di/ApiServiceModule.kt
@@ -1,11 +1,13 @@
 package com.ajailani.moodify.di
 
+import com.ajailani.moodify.data.remote.api_service.ActivityService
 import com.ajailani.moodify.data.remote.api_service.AuthService
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
+import retrofit2.create
 import javax.inject.Singleton
 
 @Module
@@ -15,4 +17,9 @@ object ApiServiceModule {
     @Singleton
     fun provideAuthService(retrofit: Retrofit): AuthService =
         retrofit.create(AuthService::class.java)
+
+    @Provides
+    @Singleton
+    fun provideActivityService(retrofit: Retrofit): ActivityService =
+        retrofit.create(ActivityService::class.java)
 }
