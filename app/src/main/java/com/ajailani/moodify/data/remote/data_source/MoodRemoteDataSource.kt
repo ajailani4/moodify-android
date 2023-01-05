2ffd5dcf4790a78359490661818da550f1eb5cc5
Ahmad Jailani <ahmadjailani604@gmail.com>
2023-01-05 16:42:24 +0700

    Add MoodRemoteDataSource

---
+++ app/src/main/java/com/ajailani/moodify/data/remote/data_source/MoodRemoteDataSource.kt
@@ -0,0 +1,20 @@
+package com.ajailani.moodify.data.remote.data_source
+
+import com.ajailani.moodify.data.remote.api_service.MoodService
+import javax.inject.Inject
+
+class MoodRemoteDataSource @Inject constructor(
+    private val moodService: MoodService
+) {
+    suspend fun getMoods(
+        page: Int,
+        size: Int,
+        month: Int,
+        year: Int
+    ) = moodService.getMoods(
+        page = page,
+        size = size,
+        month = month,
+        year = year
+    )
+}
