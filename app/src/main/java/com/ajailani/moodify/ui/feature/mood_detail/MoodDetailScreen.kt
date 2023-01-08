cfa025df92e98f00f41de922468f42a167d51580
Ahmad Jailani <ahmadjailani604@gmail.com>
2023-01-08 19:52:24 +0700

    Add state management for mood detail screen

--- app/src/main/java/com/ajailani/moodify/ui/feature/mood_detail/MoodDetailScreen.kt
+++ app/src/main/java/com/ajailani/moodify/ui/feature/mood_detail/MoodDetailScreen.kt
@@ -11,6 +11,7 @@ import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -20,13 +21,21 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import com.ajailani.moodify.R
+import com.ajailani.moodify.ui.common.UIState
+import com.ajailani.moodify.ui.feature.home.HomeEvent
+import com.ajailani.moodify.util.Formatter
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MoodDetailScreen() {
+fun MoodDetailScreen(
+    moodDetailViewModel: MoodDetailViewModel = hiltViewModel()
+) {
+    val moodDetailState = moodDetailViewModel.moodDetailState
+
     val snackbarHostState = remember { SnackbarHostState() }
 
     Scaffold(
@@ -65,55 +74,94 @@ fun MoodDetailScreen() {
                     .padding(20.dp)
                     .verticalScroll(rememberScrollState())
             ) {
-                Image(
-                    modifier = Modifier
-                        .size(100.dp)
-                        .padding(top = 10.dp)
-                        .align(Alignment.CenterHorizontally),
-                    painter = painterResource(id = R.drawable.ic_excellent_mood),
-                    contentDescription = "Mood icon"
-                )
-                Spacer(modifier = Modifier.height(30.dp))
-                Text(
-                    modifier = Modifier.align(Alignment.CenterHorizontally),
-                    text = stringResource(id = R.string.activity_that_make),
-                    style = MaterialTheme.typography.titleMedium
-                )
-                Spacer(modifier = Modifier.height(30.dp))
-                SectionCard(title = stringResource(id = R.string.activity)) {
-                    Row(verticalAlignment = Alignment.CenterVertically) {
-                        Box(
-                            modifier = Modifier
-                                .clip(CircleShape)
-                                .size(35.dp)
-                                .background(color = MaterialTheme.colorScheme.primary),
-                            contentAlignment = Alignment.Center
-                        ) {
+                when (moodDetailState) {
+                    UIState.Loading -> {
+                        Box(contentAlignment = Alignment.Center) {
+                            CircularProgressIndicator()
+                        }
+                    }
+
+                    is UIState.Success -> {
+                        moodDetailState.data?.let { mood ->
                             Image(
-                                modifier = Modifier.size(21.dp),
-                                painter = painterResource(id = R.drawable.moodify_logo),
-                                contentDescription = "Activity icon"
+                                modifier = Modifier
+                                    .size(100.dp)
+                                    .padding(top = 10.dp)
+                                    .align(Alignment.CenterHorizontally),
+                                painter = painterResource(
+                                    id = when (mood.mood) {
+                                        1 -> R.drawable.ic_terrible_mood
+                                        2 -> R.drawable.ic_bad_mood
+                                        3 -> R.drawable.ic_okay_mood
+                                        4 -> R.drawable.ic_good_mood
+                                        5 -> R.drawable.ic_excellent_mood
+                                        else -> R.drawable.ic_okay_mood
+                                    }
+                                ),
+                                contentDescription = "Mood icon"
+                            )
+                            Spacer(modifier = Modifier.height(30.dp))
+                            Text(
+                                modifier = Modifier.align(Alignment.CenterHorizontally),
+                                text = stringResource(id = R.string.activity_that_make),
+                                style = MaterialTheme.typography.titleMedium
+                            )
+                            Spacer(modifier = Modifier.height(30.dp))
+                            SectionCard(title = stringResource(id = R.string.activity)) {
+                                Row(verticalAlignment = Alignment.CenterVertically) {
+                                    Box(
+                                        modifier = Modifier
+                                            .clip(CircleShape)
+                                            .size(35.dp)
+                                            .background(color = MaterialTheme.colorScheme.primary),
+                                        contentAlignment = Alignment.Center
+                                    ) {
+                                        Image(
+                                            modifier = Modifier.size(21.dp),
+                                            painter = painterResource(id = R.drawable.moodify_logo),
+                                            contentDescription = "Activity icon"
+                                        )
+                                    }
+                                    Spacer(modifier = Modifier.width(15.dp))
+                                    Text(
+                                        text = mood.activity,
+                                        style = MaterialTheme.typography.bodyMedium
+                                    )
+                                }
+                            }
+                            Spacer(modifier = Modifier.height(20.dp))
+                            SectionCard(title = stringResource(id = R.string.note)) {
+                                Text(
+                                    text = mood.note,
+                                    style = MaterialTheme.typography.bodyMedium
+                                )
+                            }
+                            Spacer(modifier = Modifier.height(20.dp))
+                            Text(
+                                text = "${Formatter.formatDate(mood.date)}  •  ${mood.time}",
+                                style = MaterialTheme.typography.bodyMedium
                             )
                         }
-                        Spacer(modifier = Modifier.width(15.dp))
-                        Text(
-                            text = "Mendengarkan musik",
-                            style = MaterialTheme.typography.bodyMedium
-                        )
                     }
+
+                    is UIState.Fail -> {
+                        LaunchedEffect(snackbarHostState) {
+                            moodDetailState.message?.let {
+                                snackbarHostState.showSnackbar(it)
+                            }
+                        }
+                    }
+
+                    is UIState.Error -> {
+                        LaunchedEffect(snackbarHostState) {
+                            moodDetailState.message?.let {
+                                snackbarHostState.showSnackbar(it)
+                            }
+                        }
+                    }
+
+                    else -> {}
                 }
-                Spacer(modifier = Modifier.height(20.dp))
-                SectionCard(title = stringResource(id = R.string.note)) {
-                    Text(
-                        text = "This is a note",
-                        style = MaterialTheme.typography.bodyMedium
-                    )
-                }
-                Spacer(modifier = Modifier.height(20.dp))
-                Text(
-                    text = "19 December 2022  •  20:17",
-                    style = MaterialTheme.typography.bodyMedium
-                )
             }
         }
     }
