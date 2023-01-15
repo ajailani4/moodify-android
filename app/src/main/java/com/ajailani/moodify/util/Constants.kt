65275e7c616c28105c42eb9b8d17d6050bc1215b
Ahmad Jailani <ahmadjailani604@gmail.com>
2023-01-15 22:58:46 +0700

    Add statistic screen UI and its UI components

--- app/src/main/java/com/ajailani/moodify/util/Constants.kt
+++ app/src/main/java/com/ajailani/moodify/util/Constants.kt
@@ -1,8 +1,34 @@
 package com.ajailani.moodify.util
 
+import com.ajailani.moodify.domain.model.FrequentActivity
+import com.ajailani.moodify.domain.model.MoodPercentage
+
 class Constants {
     object DataStore {
         const val PREFERENCES_NAME = "moodifyPreferences"
         const val ACCESS_TOKEN_KEY = "accessToken"
     }
 }
+
+val moodPercentage = MoodPercentage(
+    excellent = 33.3f,
+    good = 25f,
+    okay = 8.3f,
+    bad = 16.7f,
+    terrible = 16.7f
+)
+
+val frequentActivities = listOf(
+    FrequentActivity(
+        activity = "Membaca",
+        count = 8
+    ),
+    FrequentActivity(
+        activity = "Bersantai",
+        count = 5
+    ),
+    FrequentActivity(
+        activity = "Bermain game",
+        count = 4
+    )
+)
