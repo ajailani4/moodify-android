bad6c9ee85854940c2faedc1054ff7cb72d02f3f
Ahmad Jailani <ahmadjailani604@gmail.com>
2023-02-01 18:35:37 +0700

    Add createNotificationChannel function in App.kt

--- app/src/main/java/com/ajailani/moodify/App.kt
+++ app/src/main/java/com/ajailani/moodify/App.kt
@@ -1,7 +1,32 @@
 package com.ajailani.moodify
 
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
+import com.ajailani.moodify.util.Constants
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class App : Application()
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        createNotificationChannel()
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationChannel = NotificationChannel(
+                Constants.NotificationChannel.FCM_ID,
+                Constants.NotificationChannel.FCM_NAME,
+                NotificationManager.IMPORTANCE_HIGH
+            ).apply {
+                description = Constants.NotificationChannel.FCM_DESCRIPTION
+            }
+
+            val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(notificationChannel)
+        }
+    }
+}
