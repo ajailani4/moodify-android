0c2b3f6a97399eeadd884086e7c1a8cf999d002b
Ahmad Jailani <ahmadjailani604@gmail.com>
2023-02-01 18:01:24 +0700

    Add FCMService

---
+++ app/src/main/java/com/ajailani/moodify/data/service/FCMService.kt
@@ -0,0 +1,14 @@
+package com.ajailani.moodify.data.service
+
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class FCMService : FirebaseMessagingService() {
+    override fun onMessageReceived(message: RemoteMessage) {
+        super.onMessageReceived(message)
+    }
+
+    override fun onNewToken(token: String) {
+        super.onNewToken(token)
+    }
+}
